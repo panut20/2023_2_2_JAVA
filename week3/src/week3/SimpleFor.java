06d4b885a55794ab93c97ad6751728780b1935b9
panut20 <mukk1110@naver.com>
2023-09-19 10:39:45 +0900

    java 3week

---
+++ week3/src/week3/SimpleFor.java
@@ -0,0 +1,13 @@
+package week3;
+
+public class SimpleFor {
+
+	public static void main(String[] args) {
+		int i=5;
+		
+		for (int j = 1; j < 10; j++) {
+			System.out.println(i+"x"+j+"="+i*j);
+		}
+	}
+
+}
